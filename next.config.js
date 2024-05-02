a63cf8eb0602102b9dd461ef7d89c33f5a8b5d6f
nyanxyz <wl77886@gmail.com>
2024-05-02 14:09:09 +0900

    fix: rewrites not working on client side navigation

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   rewrites: async () => {
-    return [
-      {
-        source: '/{/}?', // see https://github.com/vercel/next.js/issues/40549#issuecomment-1278844506
-        destination: '/Home',
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: '/',
+          destination: '/Home',
+        },
+      ],
+    };
   },
 };
 
